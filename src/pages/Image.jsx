58b21936fe8bf56e6826d5f8f63e4395fdb7de9e
Hussain Ali <iamhussainali2@gmail.com>
2024-02-02 17:33:05 +0530

    changes in image upload file

--- src/pages/Image.jsx
+++ src/pages/Image.jsx
@@ -1,51 +1,21 @@
-import axios from 'axios'
 import React, { useState } from 'react'
 
 export default function ImageUpload() {
-    const [image, setImage] = useState({ preview: '', data: '' })
-  const [status, setStatus] = useState('')
-  const handleSubmit = async (e) => {
+
+    const [image, setImage] = useState("")
+
+const imageHandler = (e) =>{
     e.preventDefault()
-    let formData = new FormData()
-    formData.append('file', image.data)
-    const response = await fetch('http://localhost:5000/api/image', {
-      method: 'POST',
-      body: formData,
-    })
-    if (response) setStatus(response.statusText)
-  }
-
-  const handleFileChange = (e) => {
-    const img = {
-      preview: URL.createObjectURL(e.target.files[0]),
-      data: e.target.files[0],
-    }
-    setImage(img)
-  }
-
-  let payload = new FormData()
-payload.append('image', File)
-axios.post('https://api.imgbb.com/1/upload?expiration=600&key=17d74797a64a4ed97d0982c31d95c223', payload)
-    .then((response) => {
-        console.log('response', response)
-        console.log('response URL', response.data.data.image.url)
-        console.log('success')
-    })
-    .catch((error) => {
-        console.log('error', error)
-        alert('try agian')
-    }) 
+    console.log(image)
+    
+}
 
   return (
-    <div className='App'>
-      <h1>Upload to server</h1>
-      {image.preview && <img src={image.preview} width='100' height='100' />}
-      <hr></hr>
-      <form onSubmit={handleSubmit}>
-        <input type='file' name='file' onChange={handleFileChange}></input>
-        <button type='submit'>Submit</button>
-      </form>
-      {status && <h4>{status}</h4>}
+    <div>
+        <form action="" onSubmit={imageHandler}>
+            <input onChange={(e) => setImage(e.target.files[0])} type="file" name="" id="" />
+            <button className='px-6 py-3 rounded-md bg-green-400 text-white'>Upload</button>
+        </form>
     </div>
   )
 }
